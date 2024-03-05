ce0a764eeb948b084c8861aad76553b1dd5677e4
tokisaki9909 <ifoster615@gmail.com>
2024-03-05 16:01:27 +0800

    regiter.js

---
+++ register.js
@@ -0,0 +1,28 @@
+console.log("Hello World!");
+console.error("ERROR HAPPEN!");
+console.warn("Warninggg!!")
+
+// Variables
+// String = "Wordsss" , "wowoowo" , "123"
+// Number = 123 , 434e, 2398.12
+// Boolean = true, false
+// Object = ["apple", "banana"], [1,2,3,4]
+             // ["apple", 3, true]
+
+// typeof
+// console.log(typeof(["apple", "banana"]))
+
+// declaration of Variables
+let name = "Lelouch"; //String
+lastname = "Lamperouge"; //String
+let age = 10; //Number
+let isProf = true; //Boolean
+let hobbies = ["Watch TV", "Reading"]; //Object
+console.log(name, age, isProf, hobbies),
+run = function (){
+    console.log("Im Running.");
+}; //Function
+
+console.table(hobbies);
+console.log("My name is " + name + " " + lastname);
+console.log(`My name is ${name} ${lastname}`);
